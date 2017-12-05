5c57ca85b6372956a55e6049507137bc29f15591
Aron Strandberg <strandberg.aron@gmail.com>
2017-12-05 16:46:32 +0100

    Add support for invoices

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  haml :index
+  haml :offer_form
+end
+
+get '/invoice' do
+  haml :invoice_form
 end
 
 get '/style.css' do
@@ -11,6 +15,10 @@ end
 
 get '/offer.png' do
  send_file 'offer.png'
+end
+
+get '/invoice.png' do
+  send_file 'invoice.png'
 end
 
 get '/offer-2018.png' do
@@ -25,6 +33,10 @@ get '/neuzeitslt.ttf' do
  send_file 'neuzeitslt.ttf'
 end
 
-get '/generate' do
+get '/generate_offer' do
   haml :offer, locals: params.map {|k, v| v == '' ? [k.to_sym, nil] : [k.to_sym, v] }.to_h
+end
+
+get '/generate_invoice' do
+  haml :invoice, locals: params.map {|k, v| v == '' ? [k.to_sym, nil] : [k.to_sym, v] }.to_h
 end
