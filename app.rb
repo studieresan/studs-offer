c53d0f9d07673cb2cdd1faa36c8c0892970d8dc7
Aron Strandberg <aron@aronstrandberg.com>
2018-05-27 17:27:23 +0200

    Remove old image route

--- app.rb
+++ app.rb
@@ -21,10 +21,6 @@ get '/invoice.png' do
   send_file 'invoice.png'
 end
 
-get '/offer-2018.png' do
- send_file 'offer-2018.png'
-end
-
 get '/generate_offer' do
   haml :offer, locals: params.map {|k, v| v == '' ? [k.to_sym, nil] : [k.to_sym, v] }.to_h
 end
